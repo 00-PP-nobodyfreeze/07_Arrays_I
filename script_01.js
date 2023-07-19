12a28a86bc2777b05cbe28df880d2f83aee59bc2
nobodyfreeze <office.fherzog@gmail.com>
2023-07-19 10:08:20 +0200

    01 getSentence() , Basic

--- script_01.js
+++ script_01.js
@@ -8,9 +8,16 @@
  */
 
 /*** 02. Theorie: Array */
+let arr;
+// arr = new Array();   // Konstruktor
+arr = [];               // Literal
+arr = [2,7,11,4];
 
-
-
+output(arr);
+output(arr.length);     // Anzahl der Elemente
+output(arr[0]);         // Index 0 (1. Position im Array)
+output(arr[4]);         // letzte Position /konkret
+output(arr(arr.length-1)); // letzte Position /allgemein
 
 /**
  *   PRAXIS
@@ -21,14 +28,17 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","der","coole","Max","Mütze"));
 
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 + 
                 PUNCT;
 
     return str;
